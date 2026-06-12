4a19c03ee263efe1e02b024d88824d105bc2c7e9
shyeon <kevinyeonkr@naver.com>
2026-06-12 16:52:44 +0900

    fix: front build error

--- jokgu-front/src/pages/users/ResetPwd.tsx
+++ jokgu-front/src/pages/users/ResetPwd.tsx
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import api from "../../api/axios"
 import { toast } from "sonner";
